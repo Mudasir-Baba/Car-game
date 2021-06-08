e194690caba15f7d9423c32ddc0ef0377bbc3048
Mohammad Mudasir Baba <captainpricegamer101@gmail.com>
2021-06-08 09:46:41 +0530

    Update car.py

---
+++ car.py
@@ -0,0 +1,24 @@
+car_is_started = False
+
+while True:
+    command = input('> ').lower()
+    if command == 'start':
+        if not car_is_started: 
+            print('Car has been started')
+            car_is_started = True
+        else:
+            print('The car has already been started!')
+
+    elif command == 'stop':
+        if car_is_started:
+            print('The car has been stopped')
+            car_is_started = False
+        else:
+            print('The car has already been stopped!')
+
+    elif command == 'quit':
+        print('Thanks for playing')
+        break
+    else:
+        print('The is not a valid command')
+
