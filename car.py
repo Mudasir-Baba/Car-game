5ded7cf950c48ed53a904cb9e80783624b62a077
Mohammad Mudasir Baba <captainpricegamer101@gmail.com>
2021-06-08 09:59:08 +0530

    Updated car.py
    Made the code shorter and better :)

--- car.py
+++ car.py
@@ -2,22 +2,22 @@ car_is_started = False
 
 while True:
     command = input('> ').lower()
-    if command == 'start' and car_is_started == False:
-        print('Car has been started')
-        car_is_started = True
+    if command == 'start':
+        if not car_is_started:
+            print('Car has been started...')
+            car_is_started = True
+        else:
+            print('The car has already been staretd!')
 
-    elif command == 'start' and car_is_started == True:
-        print('The car is already started')
-
-    elif command == 'stop' and car_is_started == True:
-        print('The car has been stopped')
-        car_is_started = False
-
-    elif command == 'stop' and car_is_started == False:
-        print('The car has already been stopped')
+    elif command == 'stop':
+        if car_is_started:
+            print('The car has been stopped...')
+            car_is_started = False
+        else:
+            print('The car has already been stopped')
 
     elif command == 'quit':
-        print('Thanks for playing')
+        print('Thanks for playing :)')
         break
     else:
         print('The is not a valid command')
